826e9425bd1e5f99ebf03ef5c2bb12b63e6222a6
Tomas Balducchi <tomasbalducchi@gmail.com>
2021-08-08 17:50:05 -0300

    acomode el nombre de la db en el .env, compila, pero no llego desde postman

--- models/userModel.js
+++ models/userModel.js
@@ -1,33 +1,40 @@
 const Sequelize = require("sequelize");
 const { sequelizeConnection } = require("../config/server/sequelizeConfig");
 
-const UserModel = sequelizeConnection.define("usuarios", {
-  idusuarios: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+const UserModel = sequelizeConnection.define(
+  "user",
+  {
+    idusuarios: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "username",
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "password",
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "city",
+    },
+    country: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "country",
+    },
   },
-  userName: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    field: "username",
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    field: "password",
-  },
-  city: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    field: "city",
-  },
-  country: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    field: "country",
-  },
-});
+  {
+    tableName: "usuarios",
+    timestamps: false,
+  }
+);
 
 module.exports = UserModel;
