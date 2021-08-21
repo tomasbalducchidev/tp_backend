375ecfe8f935cdc52f1f71526ce694e73d04c746
Tomas Balducchi <tomasbalducchi@gmail.com>
2021-08-21 16:51:14 -0300

    seguridad funcionando

--- routes/sent.route.js
+++ routes/sent.route.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const routeController = require("../common/route.controller");
-// const {required} = require('../middlewares/auth')
+const { required } = require("../middlewares/auth");
 const SentController = require("../controllers/sent.controller");
 
-router.get(
-  "/",
-  /*[required],*/ (req, res) => {
-    routeController.handleRequest(req, res, SentController.getAll);
-  }
-);
+router.get("/", [required], (req, res) => {
+  routeController.handleRequest(req, res, SentController.getAll);
+});
 
 router.post("/", (req, res) => {
   routeController.handleRequest(req, res, SentController.create);
